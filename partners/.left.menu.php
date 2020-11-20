1051d22a2b25f541edcbcc4390bce11e295b4753
CHoAoNo <chan30@mail.ru>
2020-11-20 20:29:48 +0300

    Задание для самопроверки к уроку №2

---
+++ partners/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"./", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/partners/calendar-events/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнером", 
+		"/partners/how-to-join.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
