44a7444abc2638e9c1e54bb75d2f93bbc2e3572f
CHoAoNo <chan30@mail.ru>
2020-11-23 10:25:35 +0300

    Задание для самопроверки к уроку №4.2

--- news/novaya-stranitsa2.php
+++ news/novaya-stranitsa2.php
@@ -1,15 +1,4 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Новая страница2");
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:system.auth.form", 
-	".default", 
-	array(
-		"COMPONENT_TEMPLATE" => ".default",
-		"REGISTER_URL" => "/user/registration.php",
-		"FORGOT_PASSWORD_URL" => "/user/",
-		"PROFILE_URL" => "/user/profile.php",
-		"SHOW_ERRORS" => "N"
-	),
-	false
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
