13985429a0041e5e41a86df07a8b5580a9400b99
CHoAoNo <chan30@mail.ru>
2021-01-01 15:01:54 +0300

    Блок заданий №8. 2 часть
    Создано новое почтовое событие (MY_NEW_USER) и шаблон.
    В случае успешной регистрации добавляется отправка почтового события (MY_NEW_USER) с полями: дата, логин, емэйл зарегистрированного и страница, откуда он пришел на регистрацию.
    Письмо отправлять администратору сайта (на #DEFAULT_EMAIL_FROM#, установленный в настройках).

--- local/php_interface/events.php
+++ local/php_interface/events.php
@@ -31,4 +31,33 @@ function OnBeforeUserRegisterHandler($args){
 	
   return true;
 }
+
+
+// обработчик события после регистрации
+AddEventHandler("main", "OnAfterUserRegister", "OnAfterUserRegisterHandler");
+
+function OnAfterUserRegisterHandler($arFields){
+
+	$event = new CEvent;
+	$arSiteInfo = $event->GetSiteFieldsArray(SITE_ID);
+	
+	$backUrl = ($_REQUEST["backurl"]) ?: 'нет информации';
+	
+	// если регистрация успешна, то отпарвить письмо на #DEFAULT_EMAIL_FROM#
+	if($arFields["USER_ID"]>0)
+	{
+		Bitrix\Main\Mail\Event::sendImmediate( array(
+			"EVENT_NAME" => "MY_NEW_USER",
+			"LID" => $arSiteInfo['SITE_ID'],
+			"C_FIELDS" => array(
+				"USER_ID"	=> $arFields['USER_ID'],
+				"DATE" 		=> date("d.m.Y H:i:s"),
+				"LOGIN"		=> $arFields['LOGIN'],
+				"EMAIL"		=> $arFields['EMAIL'],
+				"PAGE"		=> $backUrl
+			)
+		)); 
+	}
+	
+}
 ?>
