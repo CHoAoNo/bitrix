0e749ddc06b82b90a3cd01f3475a5d6eaa3efa77
CHoAoNo <chan30@mail.ru>
2021-01-02 16:21:47 +0300

    fix, добавлена пропущенная языковая фраза

--- local/templates/page/lang/en/header.php
+++ local/templates/page/lang/en/header.php
@@ -2,4 +2,5 @@
 $MESS['TITLE']="Site";
 $MESS['KEYWORDS']="landing page, corporate website, web development, seo promotion, turnkey SMM, contextual advertising";
 $MESS['DESCRIPTION']="description of the site";
+$MESS['WORKING_TIME']="working time"
 ?>
